913ff083f0f40adc182ec447e7b79ea9bf1e81fe
David Stef <dstef068@gmail.com>
2025-01-13 18:35:36 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './App.css'
-import App from './App.jsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Importă BrowserRouter
+import './index.css';
+import './App.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter> {/* Înconjoară aplicația cu BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
